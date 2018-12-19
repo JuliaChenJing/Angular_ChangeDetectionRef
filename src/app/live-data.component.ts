d6ece77221583842001681ab8282708148f15f8d
Julia Chen <JChen@softpro.corp>
2018-12-19 12:43:19 -0500

    works fine

---
+++ src/app/live-data.component.ts
@@ -0,0 +1,21 @@
+import { Component, ChangeDetectorRef } from '@angular/core';
+import {DataProvider} from './data-provider.model';
+
+
+@Component({
+    selector: 'live-data',
+    inputs: ['live'],
+    template: 'Data: {{dataProvider.data}}'
+})
+export class LiveData {
+    constructor(private ref: ChangeDetectorRef, private dataProvider: DataProvider) { }
+
+    set live(value: boolean) {
+        if (value) {
+            this.ref.reattach();
+        } else {
+            this.ref.detach();
+        }
+    }
+}
+
